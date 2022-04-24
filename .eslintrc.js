fefd42f9da54bfdcdb32bf6509b02d8bdce711b4
Jose <jose.d.j.d@gmail.com>
2022-04-24 19:33:24 +0200

    husky added correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,65 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019,
-    ecmaFeatures: { impliedStrict: true, jsx: true },
-  },
-  env: { es6: true, browser: true },
-  globals: {
-    console: false,
-    exports: false,
-    global: false,
-    module: false,
-    require: false,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'prettier/react',
-    'prettier/standard',
-    'prettier/babel',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-  ],
-  plugins: ['babel', 'prettier', 'react-native'],
-  rules: {
-    semi: 1,
-    'prettier/prettier': 'warn',
-    'react/prop-types': 'off',
-    'react-native/no-color-literals': 2,
-    'react-native/no-raw-text': 2,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
-    'import/order': [
-      'error',
+    parser: 'babel-eslint',
+    parserOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2019,
+      ecmaFeatures: { impliedStrict: true, jsx: true },
+    },
+    env: { es6: true, browser: true },
+    globals: {
+      console: false,
+      exports: false,
+      global: false,
+      module: false,
+      require: false,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+      'prettier/react',
+      'prettier/standard',
+      'prettier/babel',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+    ],
+    plugins: ['babel', 'prettier', 'react-native'],
+    rules: {
+      semi: 1,
+      'prettier/prettier': 'warn',
+      'react/prop-types': 'off',
+      'react-native/no-color-literals': 2,
+      'react-native/no-raw-text': 2,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always-and-inside-groups',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'unknown',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+          ],
+        },
+      ],
+    },
+    overrides: [
       {
-        'newlines-between': 'always-and-inside-groups',
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'unknown',
-          ['parent', 'sibling'],
-          'index',
-          'object',
-        ],
+        files: ['app.config.js'],
+        globals: {
+          process: true,
+        },
       },
     ],
-  },
-  overrides: [
-    {
-      files: ['app.config.js'],
-      globals: {
-        process: true,
-      },
+    settings: {
+      'import/ignore': ['node_modules/react-native/index\\.js$'],
+      react: { version: 'detect' },
     },
-  ],
-  settings: {
-    'import/ignore': ['node_modules/react-native/index\\.js$'],
-    react: { version: 'detect' },
-  },
-};
+  };
+  
